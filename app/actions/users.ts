7ee16a9832f3758efe7f5e2f4b9fa52806ea7c56
Filip Madunicky <yelidric@gmail.com>
2026-07-12 17:54:38 +0200

    feat: implement user registration with enhanced error handling and form state management

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -3,15 +3,75 @@
 import bcrypt from "bcryptjs";
 import { redirect } from "next/navigation";
 import { registerUser } from "../services/users";
+import { NeonDbError } from "@neondatabase/serverless";
+import { DrizzleError, DrizzleQueryError } from "drizzle-orm";
 
-export const createUser = async (formData: FormData) => {
+type RegisterErrorState = {
+  username?: string;
+  name?: string;
+  password?: string;
+  passwordConfirm?: string;
+};
+
+export type RegisterFormState = {
+  errors: RegisterErrorState;
+  values: {
+    username: string;
+    name: string;
+    password?: string;
+    passwordConfirm?: string;
+  };
+};
+
+export const createUser = async (
+  prevState: RegisterFormState,
+  formData: FormData,
+) => {
   const username = (formData.get("username") as string).trim();
   const name = (formData.get("name") as string).trim();
   const password = formData.get("password") as string;
+  const passwordConfirm = formData.get("passwordConfirm") as string;
 
-  const passwordHash = await bcrypt.hash(password, 10);
+  const errors: RegisterErrorState = {};
 
-  await registerUser(username, name, passwordHash);
+  if (!username || username.length < 4) {
+    errors.username = "Username must be at least 4 characters long";
+  }
+
+  if (!name || name.length < 2) {
+    errors.name = "Name must be at least 2 characters long";
+  }
+
+  if (!password || password.length < 4) {
+    errors.password = "Password must be at least 4 characters long";
+  }
+
+  if (password !== passwordConfirm) {
+    errors.passwordConfirm = "Password Confirm doesn't match with password";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+      values: { username, name },
+    };
+  }
+
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+    await registerUser(username, name, passwordHash);
+  } catch (error: unknown) {
+    if (
+      error instanceof DrizzleQueryError &&
+      error.cause instanceof NeonDbError &&
+      error.cause.code === "23505"
+    ) {
+      return {
+        errors: { username: "Username already exists" },
+        values: { username, name },
+      };
+    }
+  }
 
   redirect("/login");
 };
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,28 +1,65 @@
-import { createUser } from "../actions/users";
+"use client";
+
+import { useActionState } from "react";
+import { createUser, RegisterFormState } from "../actions/users";
+
+const initStateForm: RegisterFormState = {
+  errors: {},
+  values: {
+    username: "",
+    name: "",
+    password: "",
+    passwordConfirm: "",
+  },
+};
 
 export default function RegisterPage() {
+  const [state, formAction] = useActionState(createUser, initStateForm);
+
   return (
     <div>
       <h2>Register</h2>
 
-      <form action={createUser}>
+      <form action={formAction}>
         <div>
           <label>
             Username
-            <input type="text" name="username" />
+            <input
+              type="text"
+              name="username"
+              defaultValue={state.values.username}
+            />
           </label>
+          {state.errors.username && (
+            <p style={{ color: "red" }}>{state.errors.username}</p>
+          )}
         </div>
         <div>
           <label>
             Name
-            <input type="text" name="name" />
+            <input type="text" name="name" defaultValue={state.values.name} />
           </label>
+          {state.errors.name && (
+            <p style={{ color: "red" }}>{state.errors.name}</p>
+          )}
         </div>
         <div>
           <label>
             Password
             <input type="password" name="password" />
           </label>
+          {state.errors.password && (
+            <p style={{ color: "red" }}>{state.errors.password}</p>
+          )}
+        </div>
+        <div>
+          <label>
+            Password Confirm
+            <input type="password" name="passwordConfirm" />
+          </label>
+          {state.errors.passwordConfirm && (
+            <p style={{ color: "red" }}>{state.errors.passwordConfirm}</p>
+          )}
         </div>
         <div>
           <button type="submit">register</button>
